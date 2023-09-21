7ecd8520fc168b96f7512725ef71dbc999a4dcba
Mark <93010414+M-A-Edwards@users.noreply.github.com>
2023-09-21 21:07:35 +0530

    Add files via upload

---
+++ constants/MenuItems.js
@@ -0,0 +1,37 @@
+export default [
+    // {
+    //     name:'LoginScreen',
+    //     iconType:'Feather',
+    //     iconName:'settings'
+    // },
+    {
+        name:'Home',
+        iconType:'Feather',
+        iconName:'settings'
+    },
+    {
+        name: 'Profile',
+        iconType: 'Feather',
+        iconName: 'settings'
+    },
+    {
+        name:'Attorneys',
+        iconType:'Material',
+        iconName:'face-man-profile'
+    },
+    {
+        name:'Proceedings',
+        iconType:'Feather',
+        iconName:'settings'
+    },
+    {
+        name:'Resources',
+        iconType:'Feather',
+        iconName:'settings'
+    },
+    {
+        name:'Rehabilitation',
+        iconType:'Feather',
+        iconName:'settings'
+    }
+ ]
