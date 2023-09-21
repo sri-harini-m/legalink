c46f46b03b1fb38e6b2140e141f576de26cdf68e
b1gh3ro <saadmujeeb1008@gmail.com>
2023-09-21 23:09:34 +0530

    merged code and added name :)

--- pages/Profile.js
+++ pages/Profile.js
@@ -1,34 +1,62 @@
-import * as React from 'react';
-import { View, Text ,StyleSheet, SafeAreaView , Image , ScrollView} from "react-native";
+import * as React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+
+import { FIREBASE_AUTH } from "../firebaseConfig";
+import { signOut } from "firebase/auth";
 
 export default function ProfileScreen() {
-   return (
-      <SafeAreaView style = {styles.container}>
-         <ScrollView showsVerticalScrollIndicator={false}>
-            <View style={styles.infocontainer}>
-               <Text style={[styles.text, { fontWeight:"100", fontSize: 20}]}> Mark </Text>
-               <Text style={[styles.text, {color: "#101010",fontSize:18}]}>Criminal(Alleged)</Text>
-            </View>
-         </ScrollView>   
-      </SafeAreaView>
-   );
- }
+  let displayName = "User!";
+  const auth = FIREBASE_AUTH;
+  const user = auth.currentUser;
+  if (user !== null) {
+    displayName = user.displayName;
+  }
+  const signoutFirebase = async () => {
+    signOut(auth);
+  };
 
- const styles = StyleSheet.create({
-      container: {
-         flex:1,
-         backgroundColor: "#AEB5BC",
-         alignItems: "center",
-         justifyContent: "center"
-      },
-      text:{
-         fontFamily: "HelveticanNeue",
-         color: "#101010"
-      },
-      infocontainer: {
-        alignSelf:"center",
-        alignItems:"center",
-        marginTop:16
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.infocontainer}>
+          <Text style={[styles.text, { fontWeight: "100", fontSize: 20 }]}>
+            {" "}
+            {displayName}{" "}
+          </Text>
+          <Text style={[styles.text, { color: "#101010", fontSize: 18 }]}>
+            Criminal(Alleged)
+          </Text>
+          <TouchableOpacity onPress={signoutFirebase}>
+            <Text>signout</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
 
-      }
- })
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#AEB5BC",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontFamily: "HelveticanNeue",
+    color: "#101010",
+  },
+  infocontainer: {
+    alignSelf: "center",
+    alignItems: "center",
+    marginTop: 16,
+  },
+});
